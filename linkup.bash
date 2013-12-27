d14fbe4f6cb725aa48ba7bc18f625573d18bc916
William G Hatch <willghatch@gmail.com>
2013-12-26 20:58:56 -0700

    Add linkup script

---
+++ linkup.bash
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Script to put symlinks for dotfiles
+# assumes my dotfiles git repo is at $HOME/dotfileswgh
+
+
+dfDir=dotfileswgh
+
+dotfiles=(
+    bashrc
+    zshrc
+    vimrc
+    gitconfig
+    tmux.conf
+    muttrc
+    makepkg.conf
+)
+
+function die() {
+    echo $@
+    exit 1
+}
+
+function mklink() {
+    # $1 is the file and $2 is the symlink name
+    if [ -f $2 ]
+    then
+        mv $2 ${2}.old
+    fi
+    ln -s $1 $2
+}
+
+function linkDotfiles() {
+    cd $HOME
+    for df in ${dotfiles[@]}
+    do
+        mklink "$dfDir/$df" ".$df"
+    done
+}
+
+function linkConfigSubdirs() {
+    cdir=$HOME/$dfDir/config
+    if [ ! -d $cdir ] 
+    then
+        die "Config dir not found in proper place"
+    fi
+    cd $cdir
+    found=$(find .)
+    cd $HOME
+    for f in $found
+    do
+        orig="$dfDir/config/$f"
+        if [ -d $orig ]
+        then
+            mkdir -p ".config/$f"
+        else
+            mklink "$orig" ".config/$f"
+        fi
+    done
+}
+
+
+#######################################
+
+if [ "s-$1" = "s-dotfiles" ]
+then
+    linkDotfiles
+elif [ "s-$1" = "s-configdir" ]
+then
+    linkConfigSubdirs
+else
+    echo "usage: linkup.bash < dotfiles | configdir >"
+fi
+
