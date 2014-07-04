287403ec699cf99abc9db2bc6b90c1167d889658
William G Hatch <willghatch@gmail.com>
2014-07-04 01:21:10 -0600

    more work on emacs config

--- emacs/wghconf-evil.el
+++ emacs/wghconf-evil.el
@@ -7,4 +7,10 @@
 (define-key evil-insert-state-map "jj" 'evil-normal-state)
 (define-key evil-normal-state-map "gt" 'next-buffer)
 (define-key evil-normal-state-map "gT" 'previous-buffer)
+(define-key evil-motion-state-map "gt" 'next-buffer)
+(define-key evil-motion-state-map "gT" 'previous-buffer)
+(setq evil-normal-state-modes (append evil-emacs-state-modes evil-normal-state-modes))
+   (setq evil-emacs-state-modes nil)
+(setq evil-normal-state-modes (append evil-motion-state-modes evil-normal-state-modes))
+   (setq evil-motion-state-modes nil)
 
