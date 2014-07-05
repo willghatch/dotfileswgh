b8a52b20cbad8aa07c7104eba078869ae4b1af45
William G Hatch <willghatch@gmail.com>
2014-07-04 20:06:07 -0600

    Hmmm... fix jj issue in evil
    Apparently I don't use the letter j much, because typing it would kick
    me out of insert state.  I guess I really do need that key-chord thing
    to do that... now I will use C-h to leave insert mode.

--- emacs/wghconf-evil.el
+++ emacs/wghconf-evil.el
@@ -4,7 +4,7 @@
 (evil-mode 1)
 
 
-(define-key evil-insert-state-map "jj" 'evil-normal-state)
+(define-key evil-insert-state-map "\C-h" 'evil-normal-state)
 (define-key evil-normal-state-map "gt" 'next-buffer)
 (define-key evil-normal-state-map "gT" 'previous-buffer)
 (define-key evil-motion-state-map "gt" 'next-buffer)
