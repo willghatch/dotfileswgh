0a9b173254dce9961c73a3dbd823cacdca4c44c9
William G Hatch <whatch@micron.com>
2014-07-07 09:48:40 -0600

    change ace-jump color to be more readable with any color set

--- emacs/wghconf-ace-jump-mode.el
+++ emacs/wghconf-ace-jump-mode.el
@@ -3,4 +3,4 @@
 (require 'ace-jump-mode)
 
 (set-face-foreground 'ace-jump-face-background "#00ff00")
-(set-face-foreground 'ace-jump-face-foreground "#ff00ff")
+(set-face-foreground 'ace-jump-face-foreground "#ffffff")
