d0e59af6b09ad388103915bc539fbd0be4ef338f
William G Hatch <willghatch@gmail.com>
2014-07-10 20:43:40 -0600

    change name of emacs backup dir

--- emacs/def.el
+++ emacs/def.el
@@ -12,7 +12,7 @@
 (setq
    backup-by-copying t      ; don't clobber symlinks
    backup-directory-alist
-    '(("." . "~/.emacsbak"))    ; don't litter my fs tree
+    '(("." . "~/.bakmacs"))    ; don't litter my fs tree
    delete-old-versions t
    kept-new-versions 6
    kept-old-versions 2
