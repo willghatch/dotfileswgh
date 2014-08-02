93f8d47e8670462ff701f5049bca1e7e03ac525c
William G Hatch <willghatch@gmail.com>
2014-08-02 11:47:48 -0600

    add install script for emacs packages

---
+++ emacs/install.el
@@ -0,0 +1,12 @@
+
+(load-file "~/dotfileswgh/emacs/wghconf-package.el")
+
+(package-refresh-contents)
+
+(package-install 'evil)
+(package-install 'key-chord)
+(package-install 'ace-jump-mode)
+(package-install 'rainbow-delimiters)
+(package-install 'company)
+(package-install 'ag)
+
