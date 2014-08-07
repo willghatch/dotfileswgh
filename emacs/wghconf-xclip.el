5ff264e6484e249f7bc4ae488722fd84807fe4af
William G Hatch <whatch@micron.com>
2014-08-07 12:26:23 -0600

    add xpaste-after-char

--- emacs/wghconf-xclip.el
+++ emacs/wghconf-xclip.el
@@ -6,3 +6,8 @@
                             (filter-buffer-substring beg end)))
 (defun xpaste () (interactive)
        (insert (shell-command-to-string "xclip -o -clipboard")))
+
+(defun xpaste-after-char () (interactive)
+       (unless (eolp) (forward-char))
+       (xpaste)
+       (backward-char))
