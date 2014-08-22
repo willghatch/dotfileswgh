6e2e7011af5fc4bae55131c4dadcc94b6b48bf4f
William G Hatch <whatch@micron.com>
2014-08-22 16:54:27 -0600

    organize colors for org mode and rd

--- emacs/wghconf-rainbow-delimiters.el
+++ emacs/wghconf-rainbow-delimiters.el
@@ -7,16 +7,16 @@
 ; requires 16 color display
 
 ; (set-face-foreground FACE COLOR &optional FRAME)
-(set-face-foreground 'rainbow-delimiters-depth-1-face "#ffffff")
-(set-face-foreground 'rainbow-delimiters-depth-2-face "#00ffff")
-(set-face-foreground 'rainbow-delimiters-depth-3-face "#ff00ff")
-(set-face-foreground 'rainbow-delimiters-depth-4-face "#ffff00")
-(set-face-foreground 'rainbow-delimiters-depth-5-face "#00ff00")
-(set-face-foreground 'rainbow-delimiters-depth-6-face "#00cdcd")
-(set-face-foreground 'rainbow-delimiters-depth-7-face "#cd00cd")
-(set-face-foreground 'rainbow-delimiters-depth-8-face "#0000ee")
-(set-face-foreground 'rainbow-delimiters-depth-9-face "#00cd00")
-(set-face-foreground 'rainbow-delimiters-unmatched-face "#ff0000")
+;(set-face-foreground 'rainbow-delimiters-depth-1-face "#ffffff")
+;(set-face-foreground 'rainbow-delimiters-depth-2-face "#00ffff")
+;(set-face-foreground 'rainbow-delimiters-depth-3-face "#ff00ff")
+;(set-face-foreground 'rainbow-delimiters-depth-4-face "#ffff00")
+;(set-face-foreground 'rainbow-delimiters-depth-5-face "#00ff00")
+;(set-face-foreground 'rainbow-delimiters-depth-6-face "#00cdcd")
+;(set-face-foreground 'rainbow-delimiters-depth-7-face "#cd00cd")
+;(set-face-foreground 'rainbow-delimiters-depth-8-face "#0000ee")
+;(set-face-foreground 'rainbow-delimiters-depth-9-face "#00cd00")
+;(set-face-foreground 'rainbow-delimiters-unmatched-face "#ff0000")
  
 
 (global-rainbow-delimiters-mode)
