0a047bb8231f63facc2e1f3156066343d1f344a1
William G Hatch <whatch@micron.com>
2014-08-22 17:49:59 -0600

    todo note

--- emacs/wghconf-mode-hooks.el
+++ emacs/wghconf-mode-hooks.el
@@ -5,3 +5,20 @@
             (define-key evil-motion-state-local-map "m" 'my-buffer-menu-mode-map)
             (set-keymap-parent 'my-buffer-menu-mode-map Buffer-menu-mode-map)))
 
+
+;; TODO -- these are default values that I should deal with now
+;evil-overriding-maps
+;Value: ((Buffer-menu-mode-map)
+ ;(color-theme-mode-map)
+ ;(comint-mode-map)
+ ;(compilation-mode-map)
+ ;(grep-mode-map)
+ ;(dictionary-mode-map)
+ ;(ert-results-mode-map . motion)
+ ;(Info-mode-map . motion)
+ ;(speedbar-key-map)
+ ;(speedbar-file-key-map)
+ ;(speedbar-buffers-key-map))
+;
+;evil-intercept-maps
+;edebug-mode-map
