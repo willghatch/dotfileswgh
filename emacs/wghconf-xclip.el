94fe2d826b161f3da82862682fefdec8ccfdb4bc
William G Hatch <whatch@micron.com>
2014-08-22 16:14:11 -0600

    improve emacs xclip handling slightly

--- emacs/wghconf-xclip.el
+++ emacs/wghconf-xclip.el
@@ -5,7 +5,7 @@
        (xclip-set-selection 'clipboard
                             (filter-buffer-substring beg end)))
 (defun xpaste () (interactive)
-       (insert (shell-command-to-string "xclip -o -clipboard")))
+       (insert (shell-command-to-string "xclip -o -selection clipboard -silent")))
 
 (defun xpaste-after-char () (interactive)
        (unless (eolp) (forward-char))
