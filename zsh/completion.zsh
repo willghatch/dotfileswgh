2e90130d572eb89b12eec8da288ee5d6167ffd9d
William G Hatch <willghatch@gmail.com>
2014-09-22 17:48:57 -0600

    some work on completion in zsh

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -13,8 +13,6 @@ zstyle ':completion:*:warnings' format "%B$fg[red]%}---- no match for: $fg[white
 # Put completion into groups
 zstyle ':completion:*' group-name ''
 
-# activate approximate completion, but only after regular completion (_complete)
-zstyle ':completion:::::' completer _complete _approximate
 # limit to 2 errors
 #zstyle ':completion:*:approximate:*' max-errors 2
 # or to have a better heuristic, by allowing one error per 3 character typed
@@ -32,21 +30,63 @@ zstyle ':completion:*:approximate:*' max-errors 'reply=( $(( ($#PREFIX+$#SUFFIX)
 zstyle -e ':completion:*:default' list-colors 'reply=("${PREFIX:+=(#bi)($PREFIX:t)(?)*==36=01;31}:${(s.:.)LS_COLORS}")'
 
 
-function wgh-complete(){
+
+############### NOTE here -
+# I don't think I really understand the right way to configure zsh completers, but I'm sick of trying
+# to figure it out from the docs, so I'll just use my own workarounds here to have the settings I want
+# on different completion commands
+
+# Matchers
+function __zs-matcher-std(){
     # match on the left
     # smart-case...ish... and match numbers without using L3-shift (my layout has numbers on AltGr+these letters), and -/_
     zstyle ':completion:*'  matcher-list 'm:{a-z}={A-Z} m:{b,m,w,v,h,t,n,g,c,r,-}={0,1,2,3,4,5,6,7,8,9,_}'
-    zle expand-or-complete
+}
+function __zs-matcher-anywhere(){
+    # match anywhere in the potential completion
+    zstyle ':completion:*'  matcher-list 'm:{a-z}={A-Z} m:{b,m,w,v,h,t,n,g,c,r,-}={0,1,2,3,4,5,6,7,8,9,_} l:|=* r:|=*'
+}
+__zs-matcher-std # call this one immediately as it's the default
+
+# Completers
+function __zs-completers-std(){
+    zstyle ':completion:::::' completer _complete _approximate
+}
+function __zs-completers-gnu(){
+    zstyle ':completion:::::' completer _complete _gnu_generic
+}
+function __zs-completers-expand(){
+    zstyle ':completion:::::' completer _expand_word _expand_alias
+}
+__zs-completers-std # call immediately -- it's my default
+
+
+
+function wgh-complete(){
+    zle complete-word
 }
 zle -N wgh-complete
 
 function wgh-complete-anywhere(){
-    # match anywhere in the potential completion
-    zstyle ':completion:*'  matcher-list 'm:{a-z}={A-Z} m:{b,m,w,v,h,t,n,g,c,r,-}={0,1,2,3,4,5,6,7,8,9,_} l:|=* r:|=*'
-    zle expand-or-complete
+    __zs-matcher-anywhere
+    zle complete-word
+    __zs-matcher-std
 }
 zle -N wgh-complete-anywhere
 
+function wgh-complete-gnu(){
+    __zs-completers-gnu
+    zle complete-word
+    __zs-completers-std
+}
+zle -N wgh-complete-gnu
+
+function wgh-expand(){
+    __zs-completers-expand
+    zle complete-word
+    __zs-completers-std
+}
+zle -N wgh-expand
 
 autoload -Uz compinit
 compinit
