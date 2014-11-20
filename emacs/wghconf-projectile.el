7c0a8896d5a6449f001b96d260e8d72788c5cd8f
William G Hatch <willghatch@gmail.com>
2014-11-20 14:23:58 -0700

    emacs - projectile - do what I mean when I say find file

--- emacs/wghconf-projectile.el
+++ emacs/wghconf-projectile.el
@@ -5,5 +5,6 @@
 (define-prefix-command 'helm-projectile-map)
 (define-key projectile-command-map "h" 'helm-projectile-map)
 (define-key helm-projectile-map (kbd "h") 'helm-projectile)
-(define-key helm-projectile-map (kbd "f") 'helm-projectile-find-file)
+(define-key helm-projectile-map (kbd "f") 'helm-projectile-find-file-dwim)
+(define-key helm-projectile-map (kbd "F") 'helm-projectile-find-file)
 
