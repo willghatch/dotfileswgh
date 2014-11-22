21466d0642d3581985d930240fae8e90ecfd71f2
William G Hatch <willghatch@gmail.com>
2014-11-21 19:23:44 -0700

    switch to upstream antigen-hs

--- zsh/antigen.conf.hs
+++ zsh/antigen.conf.hs
@@ -5,17 +5,23 @@ module MyAntigen where
 import Antigen (AntigenConfiguration (..)
               , bundle
               , antigen
+              , ZshPlugin (..)
+              , antigenSourcingStrategy
+              , filePathsSourcingStrategy
               , developFromFileSystem)
 import Shelly (shelly)
 
 bundles =
   [ bundle "zsh-users/zsh-history-substring-search"
   , bundle "hchbaw/opp.zsh.git"
-  , bundle "alfredodeza/zsh-plugins.git vi/zle_vi_visual.zsh"
-  , bundle "robbyrussell/oh-my-zsh plugins/wd"
-  , bundle "robbyrussell/oh-my-zsh plugins/cabal"
-  , bundle "robbyrussell/oh-my-zsh plugins/catimg"
-  , bundle "robbyrussell/oh-my-zsh plugins/cp"
+  , (bundle "alfredodeza/zsh-plugins.git")
+     {sourcingStrategy = filePathsSourcingStrategy ["vi/zle_vi_visual.zsh"]}
+  , (bundle "robbyrussell/oh-my-zsh") { sourcingLocations =
+        [ "plugins/wd"
+        , "plugins/cabal"
+        , "plugins/catimg"
+        , "plugins/cp"
+        ]}
   , bundle "zsh-users/zsh-syntax-highlighting"
   , bundle "zsh-users/zaw"
   , bundle "willghatch/zsh-zaw-extras"
