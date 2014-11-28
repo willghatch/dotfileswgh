301eaf88c72c3068acaed6d424e081777699cb77
William G Hatch <willghatch@gmail.com>
2014-11-28 13:06:27 -0700

    add script to install stuff I need with cabal for xmonad

---
+++ cabal-needs.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# these are because apparently cabal can't figure out build dependencies...
+cabal install happy # for c2hs... and lots of stuff
+cabal install alex # just in case
+
+cabal install xmonad
+cabal install xmonad-contrib
+
+cabal install c2hs # for alsa stuff
+cabal install alsa-core
+cabal install alsa-mixer
+cabal install libmpd
+
+cabal install xmobar --flags="with_alsa,with_mpd"
+
+
+xmonad --recompile
+
