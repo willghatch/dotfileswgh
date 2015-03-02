19870956cba39d1cc1cece28c06357f2e831c0e0
William G Hatch <willghatch@gmail.com>
2015-03-02 10:41:54 -0700

    emacs - make xcopy function consistent with regular copy funcs

--- emacs/xclip-conf.el
+++ emacs/xclip-conf.el
@@ -3,7 +3,12 @@
 
 (defun xcopy (beg end) (interactive "r")
        (xclip-set-selection 'clipboard
-                            (filter-buffer-substring beg end)))
+                            (filter-buffer-substring beg end))
+       (let ((eol (eolp))
+             (point-at-region-end (equal end (point))))
+         (call-interactively 'evil-normal-state)
+         (if (and point-at-region-end (not eol))
+             (backward-char))))
 (defun xpaste () (interactive)
        (insert (shell-command-to-string "xclip -o -selection clipboard -silent")))
 
