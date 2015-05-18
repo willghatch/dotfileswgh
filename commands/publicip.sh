ec18054326e6ab41a1c9b3b596c90a3f15b6970c
William G Hatch <willghatch@gmail.com>
2015-05-17 23:30:07 -0600

    add script to get public ip

---
+++ commands/publicip.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# get my public IP address
+
+
+if which dig >/dev/null 2>&1; then
+    # using dns, if possible.  Note, dig is in the dnsutils package in Arch.
+    dig +short myip.opendns.com @resolver1.opendns.com
+else
+
+    # using web services
+
+    # there are various alternatives
+    # instead of curl you could use 
+    getpage="curl"
+    #getpage="wget -qO-"
+    
+    $getpage icanhazip.com
+    #$getpage ipv4.icanhazip.com
+    #$getpage ipv6.icanhazip.com
+    #$getpage ident.me
+    #$getpage v4.ident.me
+    #$getpage v6.ident.me
+    #$getpage ipecho.net/plain
+    #$getpage ifconfig.me
+    #$getpage -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'
+
+fi
