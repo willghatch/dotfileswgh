27b8215ab4d3bfc340bc7af654abb238d0dbfb6f
William G Hatch <willghatch@gmail.com>
2015-05-26 11:02:59 -0600

    add arch package install script from pri

---
+++ archpackages.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+pm (){
+    pacman -S --needed --noconfirm "$@"
+}
+
+pm base base-devel openssh net-tools wpa_supplicant networkmanager wireless_tools
+pm git
+pm zsh vim emacs tmux elinks
+
+pm xorg konsole xterm xorg-xinit arandr bashrun
+pm python python2 python-pip python2-pip ghc cabal-install racket nodejs npm
+pm firefox
+pm sshfs zip unzip evince tig
+
+pm \
+awesome \
+lxterminal \
+xfce4-terminal \
+chromium \
+xterm \
+xscreensaver \
+unclutter \
+vorbis-tools \
+vlc \
+wget \
+mpd \
+mpc \
+ncmpc \
+espeak \
+qemu \
+network-manager-applet \
+mlocate \
+ctags \
+clisp \
+alsa-utils \
+abcde \
+lxappearance \
+fortune-mod \
+cowsay \
+texlive-bin
+
