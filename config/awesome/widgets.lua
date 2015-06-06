1674de3d48048024a1e3a7394b94eb0bf8b760dc
William G Hatch <willghatch@gmail.com>
2015-06-06 15:06:37 -0600

    awesome - comment out CPU widget, because it doesn't seem to work.

--- config/awesome/widgets.lua
+++ config/awesome/widgets.lua
@@ -42,15 +42,16 @@ cpuTextWidget = wibox.widget.textbox()
 -- Register widget
 vicious.register(cpuTextWidget, vicious.widgets.cpu, "CPU: $1%")
 
--- Initialize widget
-cpuwidget = awful.widget.graph()
--- Graph properties
-cpuwidget:set_width(50)
-cpuwidget:set_background_color("#494B4F")
-cpuwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 10,0 }, stops = { {0, "#FF5656"}, {0.5, "#88A175"},
-                         {1, "#AECF96" }}})
--- Register widget
-vicious.register(cpuwidget, vicious.widgets.cpu, "$1")
+-- this CPU widget is full of crap.
+---- Initialize widget
+--cpuwidget = awful.widget.graph()
+---- Graph properties
+--cpuwidget:set_width(50)
+--cpuwidget:set_background_color("#494B4F")
+--cpuwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 10,0 }, stops = { {0, "#FF5656"}, {0.5, "#88A175"},
+--                         {1, "#AECF96" }}})
+---- Register widget
+--vicious.register(cpuwidget, vicious.widgets.cpu, "$1")
 
 --volume widget
 volume_widget = wibox.widget.textbox()
@@ -184,7 +185,7 @@ for s = 1, screen.count() do
    right_layout:add(mpdwidget)
    right_layout:add(volume_widget)
    right_layout:add(cpuTextWidget)
-   right_layout:add(cpuwidget)
+   --right_layout:add(cpuwidget)
    right_layout:add(memTextWidget)
    right_layout:add(memwidget)
    right_layout:add(batteryTextWidget)
