e6e985795fc552c0782be391a0e24ef0d0dc0a94
William G Hatch <willghatch@gmail.com>
2015-06-08 17:30:34 -0600

    awesome - edit binding

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -63,7 +63,9 @@ editor_cmd = terminal .. " -e " .. editor
 -- If you do not like this or do not have such a key,
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
+superkey = "Mod4"
+hyperkey = "Mod2"
+modkey = superkey
 
 -- which modifier states will be ignored by bindings?
 --awful.key.ignore_modifiers = {"Lock", "Mod2"}
@@ -235,7 +237,7 @@ genGlobalKeys = function(modkey)
       -- Launch Programs
       awful.key({ modkey,           }, "r",     function () awful.util.spawn("bashrun")  end),
       awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
-      awful.key({ modkey, "Shift"   }, "Return", function () awful.util.spawn(terminal2) end),
+      awful.key({ modkey, "Control" }, "Return", function () awful.util.spawn(terminal2) end),
 
       --awful.key({ modkey, "Mod1"    }, "b", function () awful.util.spawn("dwb") end),
       --awful.key({ "Control", "Mod1" }, "l", function () awful.util.spawn("xscreensaver-command -lock") end),
