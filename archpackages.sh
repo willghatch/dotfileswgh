33ab89a0c6599cf531f7e4d3b237d663caf71118
William G Hatch <willghatch@gmail.com>
2015-09-06 10:36:21 -0600

    move things around a bit in archpackages

--- archpackages.sh
+++ archpackages.sh
@@ -6,10 +6,20 @@ pm (){
     pacman -S --needed --noconfirm "$@"
 }
 
-pm base base-devel openssh net-tools wpa_supplicant networkmanager wireless_tools
-pm git
-pm zsh vim emacs tmux elinks
+# get basic packages
+pm base base-devel
+# networking
+pm openssh net-tools wpa_supplicant networkmanager wireless_tools
 
+# get my basic tools
+pm git
+pm zsh
+pm vim
+pm tmux
+pm elinks
+pm emacs
+
+# next tier of basic things
 pm xorg konsole xterm xorg-xinit arandr bashrun ttf-dejavu xclip
 pm python python2 python-pip python2-pip ghc cabal-install racket nodejs npm
 pm firefox
@@ -62,4 +72,3 @@ texlive-bin
 pm avr-binutils avr-gcc avr-libc
 # for mkfs.vfat
 pm dosfstools
-
