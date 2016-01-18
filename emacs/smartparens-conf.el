2be2b1582c91869b4f4dfb8f6264d4bc5b61971f
William G Hatch <willghatch@gmail.com>
2016-01-18 13:14:30 -0700

    add more delimiters to smartparens

--- emacs/smartparens-conf.el
+++ emacs/smartparens-conf.el
@@ -7,3 +7,40 @@
 ;; add some pairs that seem to be lacking
 (sp-local-pair 'racket-mode "#|" "|#")
 (setq sp-sexp-prefix '((racket-mode regexp "#?[`',]@?")))
+
+;; add other types of pairs things
+;; a lot of these sorts of things are at:
+;; http://xahlee.info/comp/unicode_matching_brackets.html
+;;
+;; TODO - make these work with text object selection, `cs<delimiter>` replacement, etc
+(sp-pair "«" "»")
+(sp-pair "‹" "›")
+(sp-pair "｢" "｣")
+(sp-pair "『" "』")
+(sp-pair "“" "”")
+(sp-pair "‘" "’")
+(sp-pair "❛" "❜")
+(sp-pair "❝" "❞")
+(sp-pair "⦗" "⦘")
+(sp-pair "〖" "〗")
+(sp-pair "⟦" "⟧")
+(sp-pair "⌈" "⌉")
+(sp-pair "⌊" "⌋")
+(sp-pair "⦇" "⦈")
+(sp-pair "⦉" "⦊")
+(sp-pair "⸢" "⸣")
+(sp-pair "⸤" "⸥")
+(sp-pair "〈" "〉")
+(sp-pair "⦑" "⦒")
+(sp-pair "⧼" "⧽")
+(sp-pair "⁅" "⁆")
+(sp-pair "❰" "❱")
+(sp-pair "❮" "❯")
+(sp-pair "⟅" "⟆")
+(sp-pair "⦓" "⦔")
+(sp-pair "⦕" "⦖")
+(sp-pair "⸦" "⸧")
+(sp-pair "᚛" "᚜")
+(sp-pair "༺" "༻")
+(sp-pair "༼" "༽")
+
