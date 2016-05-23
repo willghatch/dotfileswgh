d109951048c87f740d76aa182e82778d379700af
William G Hatch <william@hatch.uno>
2016-05-23 12:39:11 -0600

    awesome: reorder layouts

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -92,13 +92,13 @@ awful.key.ignore_modifiers = {"Lock"}
 -- Table of layouts to cover with awful.layout.inc, order matters.
 local layouts =
    {
-      awful.layout.suit.tile,
-      awful.layout.suit.tile.left,
-      awful.layout.suit.tile.bottom,
       awful.layout.suit.fair,
-      awful.layout.suit.spiral.dwindle,
-      awful.layout.suit.max,
       awful.layout.suit.floating,
+      awful.layout.suit.tile,
+      --awful.layout.suit.tile.left,
+      awful.layout.suit.tile.bottom,
+      --awful.layout.suit.spiral.dwindle,
+      awful.layout.suit.max,
 
       --awful.layout.suit.tile.top,
       --awful.layout.suit.fair.horizontal,
