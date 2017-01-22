df2d4fc61811803c85303e0e2668f2221611f853
William G Hatch <william@hatch.uno>
2017-01-22 14:04:36 -0700

    add package scripts for lineage os building

---
+++ packages/lineage-build-pkgs-arch.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+pm (){
+    pacman -S --needed --noconfirm "$@"
+}
+
+
+# pacman packages
+pm gcc-multilib git gnupg flex bison gperf sdl wxgtk squashfs-tools curl ncurses zlib schedtool perl-switch zip unzip libxslt python2-virtualenv bc rsync lib32-zlib lib32-ncurses lib32-readline
+
+# lineage-os additionally needs these
+pm xml2 lzop pngcrush schedtool squashfs-tools lzop imagemagick
+
+
+# cyanogenmod/lineage 14 and android 7 switched to java 8, and officially requires openjdk
+pm jdk8-openjdk
+
+echo "installed official repo packages needed, remember to install AUR packages"
+
---
+++ packages/lineage-build-pkgs-aur.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# these will probably have key signing issues unless you import a key...
+# see AUR page for details...
+pacaur -S ncurses5-compat-libs
+pacaur -S lib32-ncurses5-compat-libs
+
