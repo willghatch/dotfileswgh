fd567099e63249d873b587dacde651b2e8b3b297
William G Hatch <william@hatch.uno>
2017-02-01 15:30:24 -0700

    sway: add config (that currently works on git version)

---
+++ commands/i3-make-empty-workspace.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# command from the internet...
+i3-msg workspace $(($(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1) + 1))
+vlaunch terminal &
+
