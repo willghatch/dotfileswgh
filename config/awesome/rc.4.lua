e1efdc162fa969ff8ef7f604e36fc59cb075a811
William G Hatch <william@hatch.uno>
2017-02-24 15:32:06 -0700

    awesomewm: get rid of title bars!

--- config/awesome/rc.4.lua
+++ config/awesome/rc.4.lua
@@ -640,7 +640,7 @@ awful.rules.rules = {
       }, properties = { floating = true }},
 
     -- Add titlebars to normal clients and dialogs
-    { rule_any = {type = { "normal", "dialog" }
+    { rule_any = {type = { "dialog" }
       }, properties = { titlebars_enabled = true }
     },
 
