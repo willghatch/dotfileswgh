6c20e90b7474287cc4fd9e831ea9781207044bae
William G Hatch <william@hatch.uno>
2018-01-03 21:08:40 -0700

    i3-make-empty-workspace.sh -- fix so it works with sway again

--- commands/i3-make-empty-workspace.sh
+++ commands/i3-make-empty-workspace.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+msg=i3-msg
+if [[ -n "$SWAYSOCK" ]]; then
+    msg=swaymsg
+fi
+
+
 # command from the internet...
-i3-msg workspace $(($(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1) + 1))
+$msg workspace $(($($msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1) + 1))
 vlaunch terminal &
 
