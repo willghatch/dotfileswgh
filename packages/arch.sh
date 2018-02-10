0c844c0522254ee0a8e330849ccf8c2c57f2fada
William G Hatch <william@hatch.uno>
2018-02-09 21:37:13 -0700

    arch: add vdpau package

--- packages/arch.sh
+++ packages/arch.sh
@@ -26,6 +26,8 @@ pm emacs
 
 # next tier of basic things
 pm xorg konsole xterm xorg-xinit arandr bashrun ttf-dejavu xclip
+# I guess I need vdpau to have hardware acceleration for video
+pm mesa-vdpau
 pm python python2 python-pip python2-pip racket nodejs npm
 #pm ghc cabal-install
 pm firefox
