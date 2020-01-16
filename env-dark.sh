883e7f0547c836175ddaf9a53b3c3bad02eeaf65
William G Hatch <william@hatch.uno>
2020-01-16 14:21:14 -0700

    simplify dark/light environment stuff

---
+++ env-dark.sh
@@ -0,0 +1,4 @@
+export WGH_THEME_DARK_OR_LIGHT="dark"
+export GTK_THEME=Adwaita:dark
+export GTK2_RC_FILES=$DOTFILESWGH/gtk2rc-dark
+export QT_STYLE_OVERRIDE=Adwaita-Dark
---
+++ env-light.sh
@@ -0,0 +1,4 @@
+export WGH_THEME_DARK_OR_LIGHT="light"
+export GTK_THEME=Adwaita:light
+export GTK2_RC_FILES=$DOTFILESWGH/gtk2rc-light
+export QT_STYLE_OVERRIDE=Adwaita
