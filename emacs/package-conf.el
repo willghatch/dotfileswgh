33bc97bf156b757d69acbf2b34acb3b0471d2ba7
William G Hatch <william@hatch.uno>
2020-07-27 17:21:34 -0600

    emacs: update melpa url

--- emacs/package-conf.el
+++ emacs/package-conf.el
@@ -16,7 +16,7 @@
 
 (require 'package)
 (setq package-archives '(
-                         ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("melpa" . "http://melpa.org/packages/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ;("marmalade" . "http://marmalade-repo.org/packages/")
                          ))
