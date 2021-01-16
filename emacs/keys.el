643d5a681a97b65459e448c2367f527b19b8511f
William G Hatch <william@hatch.uno>
2021-01-15 20:47:08 -0700

    emacs: one more slight modification to keybindings

--- emacs/keys.el
+++ emacs/keys.el
@@ -345,7 +345,11 @@
 (mkmap "gr" 'evil-ace-jump-word-mode)
 (mkmap "gc" 'evil-ace-jump-char-mode)
 (mkmap "gf" 'evil-ace-jump-line-mode)
-(mkmap "gdd" 'evil-goto-definition)
+;; TODO - I should have a binding for “go to definition in this frame”
+;;        as well as “go to definition in other frame”.  It's nice to
+;;        start in a different frame from my work, but then probably stay
+;;        in that frame for chasing down a series of definitions...
+(mkmap "gdd" 'xref-find-definitions-other-frame)
 (mkmap "gdp" 'pop-tag-mark)
 (nkmap " yc" 'xcopy)
 (nkmap " Pc" 'xpaste)
