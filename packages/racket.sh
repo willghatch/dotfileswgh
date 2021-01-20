192c40e8a48a46e8bc02863b98181cb3e20c914c
William G Hatch <william@hatch.uno>
2021-01-20 14:47:29 -0700

    package install script: add package

--- packages/racket.sh
+++ packages/racket.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 raco pkg install --batch --deps search-auto --skip-installed \
-     rash the-unicoder rackterm readline-gpl k-infix frog gregor
+     rash rash-demos the-unicoder rackterm readline-gpl k-infix frog gregor
