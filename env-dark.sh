550340e72a07a7db647830a1a302d56770ece7e7
William G Hatch <william@hatch.uno>
2022-02-12 16:01:56 -0700

    fuss with light/dark stuff a bit

--- env-dark.sh
+++ env-dark.sh
@@ -1,6 +1,6 @@
-#export WGH_THEME_DARK_OR_LIGHT="dark"
+export WGH_THEME_DARK_OR_LIGHT="dark"
 
 # Newer GTK supports getting theme over gsettings, which can live-update.  So don't set environment variables.
-#export GTK_THEME=Adwaita:dark
-#export GTK2_RC_FILES=$DOTFILESWGH/gtk2rc-dark
-#export QT_STYLE_OVERRIDE=Adwaita-Dark
+export GTK_THEME=Adwaita:dark
+export GTK2_RC_FILES=$DOTFILESWGH/gtk2rc-dark
+export QT_STYLE_OVERRIDE=Adwaita-Dark
--- env-light.sh
+++ env-light.sh
@@ -1,6 +1,6 @@
-#export WGH_THEME_DARK_OR_LIGHT="light"
+export WGH_THEME_DARK_OR_LIGHT="light"
 
 # Newer GTK supports getting theme over gsettings, which can live-update.  So don't set environment variables.
-#export GTK_THEME=Adwaita:light
-#export GTK2_RC_FILES=$DOTFILESWGH/gtk2rc-light
-#export QT_STYLE_OVERRIDE=Adwaita
+export GTK_THEME=Adwaita:light
+export GTK2_RC_FILES=$DOTFILESWGH/gtk2rc-light
+export QT_STYLE_OVERRIDE=Adwaita
