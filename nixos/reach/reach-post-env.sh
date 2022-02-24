729805c67937b4c49f2644fc6bcefd333797c4ad
William G Hatch <william@hatch.uno>
2022-02-23 20:52:20 -0700

    improve once more reach dev environment scripts

--- nixos/reach/reach-post-env.sh
+++ nixos/reach/reach-post-env.sh
@@ -7,8 +7,11 @@ unset NIX_SSL_CERT_FILE
 # Make sure `goal` is on $PATH.
 binpath="$XDG_RUNTIME_DIR/$CURRENT_DEV_MODE/bin"
 mkdir -p $binpath
-ln -sf "$DEV_MODE_REACH/scripts/goal-devnet" "$binpath/goal"
 PATH=$PATH:$binpath
+if [[ -n "$DEV_MODE_REACH" ]]; then
+    PATH=$PATH:$DEV_MODE_REACH
+    ln -sf "$DEV_MODE_REACH/scripts/goal-devnet" "$binpath/goal"
+fi
 
 
 exec zsh
