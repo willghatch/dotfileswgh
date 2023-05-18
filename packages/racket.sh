ddd380b9d87e155630441bcc896749c85db33a6d
William G Hatch <william@hatch.uno>
2023-05-18 16:40:23 -0600

    racket packages: don't install frog, because now I'm using a fork

--- packages/racket.sh
+++ packages/racket.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 raco pkg install --batch --deps search-auto --skip-installed \
-     rash rash-demos the-unicoder rackterm readline-gpl k-infix frog gregor sha
+     rash rash-demos the-unicoder rackterm readline-gpl k-infix gregor sha
