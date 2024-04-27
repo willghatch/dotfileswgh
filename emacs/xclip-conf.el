3f01a703e5999b1db5d880debaa56a2aa96c708f
William G Hatch <william@hatch.uno>
2024-04-27 11:16:24 -0600

    emacs: fix xclip for wayland

--- emacs/xclip-conf.el
+++ emacs/xclip-conf.el
@@ -2,15 +2,20 @@
 (defun init-xclip-conf ()
   (unless init-xclip-conf-done
     (require 'xclip)
-    (setq xclip-method 'xclip)
-    (when (equal system-type 'darwin)
-      (setq xclip-method 'pbpaste))
+    (cond ((equal system-type 'darwin)
+           (setq xclip-method 'pbpaste))
+          ((equal (getenv "XDG_SESSION_TYPE") "wayland")
+           (setq xclip-method 'wl-copy))
+          (t (setq xclip-method 'xclip)))
     (setq init-xclip-conf-done t)))
 
 (defun xcopy (beg end) (interactive "r")
        (init-xclip-conf)
-       (xclip-set-selection 'clipboard
-                            (filter-buffer-substring beg end))
+       ;; the xclip package doesn't seem to be working on wayland right now, let's hack around it.
+       (if (equal xclip-method 'wl-copy)
+           (call-process-region (region-beginning) (region-end) "wl-copy")
+         (xclip-set-selection 'clipboard
+                            (filter-buffer-substring beg end)))
        (let ((eol (eolp))
              (point-at-region-end (equal end (point))))
          (call-interactively 'evil-normal-state)
@@ -25,7 +30,10 @@
 (defun xpaste () (interactive)
        (init-xclip-conf)
        (let ((xclip-select-enable-clipboard t))
-         (insert (xclip-get-selection 'clipboard))))
+         ;; the xclip package doesn't seem to be working on wayland right now, let's hack around it.
+         (if (equal xclip-method 'wl-copy)
+             (call-process "wl-paste" nil t nil "-n")
+           (insert (xclip-get-selection 'clipboard)))))
 
 (defun xpaste-after-char () (interactive)
        (init-xclip-conf)
