6ee945b2ba38c5864656d3654c90125a04f23615
William G Hatch <william@hatch.uno>
2024-06-17 08:37:43 -0600

    hyprland: add config changes written recently

--- commands/hyprland-make-workspace.sh
+++ commands/hyprland-make-workspace.sh
@@ -6,11 +6,22 @@ if [[ "$@" =~ "--help" ]]; then
     exit 1
 fi
 
+if [[ "$1" = "--move-current-window" ]]; then
+    shift
+    windowToMove="$(hyprctl -j activewindow | jq .pid)"
+fi
+
 newWorkspaceNumber="$(( $(hyprctl workspaces | grep "workspace ID" | cut -d " " -f 3 | sort -rn | head -1) + 1 ))"
 
 hyprctl dispatch workspace "${newWorkspaceNumber}"
 
 # command from the internet...
+
+if [[ -n "$windowToMove" ]]; then
+    echo "in windowToMove case" > /tmp/log
+    hyprctl dispatch movetoworkspace "${newWorkspaceNumber},pid:${windowToMove}"
+    echo hyprctl dispatch movetoworkspace "${newWorkspaceNumber},pid:${windowToMove}" >>/tmp/log
+fi
 
 if [[ -n "$1" ]]; then
     #vlaunch terminal &
