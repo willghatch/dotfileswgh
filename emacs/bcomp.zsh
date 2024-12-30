4c8a8de2ba3ace03fd65739313b945d26e1aeab2
William G Hatch <william@hatch.uno>
2024-12-30 10:35:25 -0700

    emacs: add comments from a quick experiment with native compilation

--- emacs/bcomp.zsh
+++ emacs/bcomp.zsh
@@ -4,5 +4,6 @@
 
 df=$DOTFILESWGH
 
-emacs -l $df/emacs/def.el -batch -f batch-byte-compile $df/emacs/*.el
+#emacs -l $df/emacs/def.el -batch -f batch-byte-compile $df/emacs/*.el
+#emacs -l $df/emacs/def.el -batch -f batch-native-compile $df/emacs/{def,*-conf,keys,init-helpers,estate-core,estate-vim-like-states,scratch-message,scribble-funcs,sensitive-mode,text-object-stuff,tree-walk-smartparens-integration,tree-walk,vfuncs,wgh-theme}.el
 
