8ac6c86c75879e082fe47016588538a076543513
William G Hatch <william@hatch.uno>
2025-01-14 16:33:52 -0700

    env-basic: detect dotfiles when not given, allowing them to be in rootgit

--- env-basic.sh
+++ env-basic.sh
@@ -1,5 +1,14 @@
 # This is the necessary environment that my various dotfiles reference
 
+if [ -z "$DOTFILESWGH" ]; then
+    if [[ -d "$HOME/dotfileswgh" ]]; then
+        export DOTFILESWGH="$HOME/dotfileswgh"
+    elif [[ -d /rootgit/base.rootgit/dotfileswgh ]]; then
+        export DOTFILESWGH=/rootgit/base.rootgit/dotfileswgh
+    fi
+fi
+
 export DOTFILESWGH="${DOTFILESWGH:-$HOME/dotfileswgh}"
+
 #PATH=$DOTFILESWGH/commands:$DOTFILESWGH/commands/aliases:$PATH
 
