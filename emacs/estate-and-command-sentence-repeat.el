abeb1d18b94cf9e4368382eb6bba64e032fd8995
William G Hatch <william@hatch.uno>
2025-01-19 18:01:36 -0700

    emacs estate-and-command-sentence-repeat eacsr - fix prefix arg modification detection

--- emacs/estate-and-command-sentence-repeat.el
+++ emacs/estate-and-command-sentence-repeat.el
@@ -11,17 +11,16 @@
 (setq eacsr--editing-groups-ring (make-ring 100))
 
 (defun eacsr--pre-command ()
-  (setq eacsr--pre-command-prefix-arg-state current-prefix-arg))
+  (setq eacsr--pre-command-prefix-arg-state prefix-arg))
 
-;; TODO - maybe have a predicate for splitting command groups, then this could be something that is part of estate-mode without command sentence or command-sentence without estate mode or together or whatever.
+;; TODO - maybe have a predicate for splitting command groups, then this could be something that is part of estate-mode without command sentence or command-sentence without estate mode or together or whatever.  Also I want to be able to change it dynamically.  Eg. I want a function that sets up recording until some condition is met, then return to the previous/default predicate.  Eg. have a key that means “record all commands until back in normal state with an empty command sentence after an edit has been made”, which would allow chaining some prefix movements to be part of the recording.  This would be slightly less setup than an anonymous keyboard macro (one less key since you don't have to manually stop the recording) and maybe easier to nest with keyboard macros, since there is one level less that you need to keep track of register names for.
 ;; TODO - when repeating, it duplicates the command in the ring.  Probably I should filter it.
 ;; TODO - also I want to be able to filter to reject certain commands.  Eg. I want to filter out undo.
 (defun eacsr--post-command ()
   (setq eacsr--current-groups (cons (this-command-all-keys) eacsr--current-groups))
   (when (and (equal estate-state 'normal)
              (equal command-sentence-current-sentence nil)
-             ;; TODO - I want to include commands that modify the prefix argument with the overall command, but I'm having trouble identifying which commands do that...
-             ;;(equal eacsr--pre-command-prefix-arg-state current-prefix-arg)
+             (equal eacsr--pre-command-prefix-arg-state prefix-arg)
              )
     (let ((finalized-group (reverse eacsr--current-groups)))
       (ring-insert eacsr--all-groups-ring finalized-group)
