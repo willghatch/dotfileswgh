0b3af653ba440b0cd143ee9096372c764ff55564
William G Hatch <william@hatch.uno>
2025-04-24 20:35:59 -0600

    fuss with hyprland script

--- commands/hyprland-make-workspace.sh
+++ commands/hyprland-make-workspace.sh
@@ -11,7 +11,25 @@ if [[ "$1" = "--move-current-window" ]]; then
     windowToMove="$(hyprctl -j activewindow | jq .pid)"
 fi
 
-newWorkspaceNumber="$(( $(hyprctl workspaces | grep "workspace ID" | cut -d " " -f 3 | sort -rn | head -1) + 1 ))"
+function firstMissingNumber() {
+    last_number=0
+    missing_number_found=false
+
+    while read -r number; do
+        if [[ $number -gt $(($last_number + 1)) && "$missing_number_found" = false ]]; then
+            echo $(($last_number + 1))
+            missing_number_found=true
+            break
+        fi
+        last_number=$number
+    done
+    if [[ "$missing_number_found" = false ]]; then
+        echo $(($last_number + 1))
+    fi
+}
+
+newWorkspaceNumber="$(( $(hyprctl workspaces | grep "workspace ID" | cut -d " " -f 3 | sort -rn | head -1) + 1 ))" # This one is annoying because when a workspace is missing, it keeps incrementing the numbers, then there are holes.
+#newWorkspaceNumber="$(hyprctl workspaces | grep "workspace ID" | cut -d " " -f 3 | sort -n | firstMissingNumber)" # This one is annoying because the new workspace goes in the middle of the existing workspaces, where the missing number was, whereas I want the new workspace to appear on the right.
 
 hyprctl dispatch workspace "${newWorkspaceNumber}"
 
