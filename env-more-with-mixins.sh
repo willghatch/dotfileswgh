bb3d670376f6274834dbde5e495288530aadf145
William G Hatch <william@hatch.uno>
2025-05-01 12:28:56 -0600

    fuss with env sourcing

---
+++ env-more-with-mixins.sh
@@ -0,0 +1,17 @@
+# Requires $HOME and $DOTFILESWGH be set
+
+source $DOTFILESWGH/env-more.sh
+
+# source other dotfileswgh mixin envs if they exist
+if [ -f "$DOTFILESWGH_PRI/env.sh" ]; then
+    source "$DOTFILESWGH_PRI/env.sh"
+fi
+if [ -f "$DOTFILESWGH_ROOTGIT/env.sh" ]; then
+    source "$DOTFILESWGH_ROOTGIT/env.sh"
+fi
+if [ -f "$DOTFILESWGH_DOTLOCAL/env.sh" ]; then
+    source "$DOTFILESWGH_DOTLOCAL/env.sh"
+fi
+if [ -f "$DOTFILESWGH_PRI_DOTLOCAL/env.sh" ]; then
+    source "$DOTFILESWGH_PRI_DOTLOCAL/env.sh"
+fi
